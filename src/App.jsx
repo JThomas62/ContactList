f860809a21739a8a3bc407bb0019ecabcc06f19b
JThomas62 <jacobt257@gmail.com>
2023-09-12 20:41:44 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Your Blank Application</h1>
+      <div className="card">
+        {/* Your content goes here */}
+      </div>
+    </>
+  )
+}
+
+export default App
